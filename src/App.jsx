848e654fa23586d928816d20f476dcbe469404f7
zorichDev <ffranzor@yahoo.com>
2025-09-30 16:34:49 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import ScholarTrack from './assets/RPro'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <ScholarTrack/>
+    </>
+  )
+}
+
+export default App
